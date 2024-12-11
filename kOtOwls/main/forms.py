98bde946ade96ce4e3980b6e443263d7c50a0749
Leonid <148010246+Leonid-Perkin@users.noreply.github.com>
2024-12-11 11:56:15 +0300

    Сделан фронт для загрузки фото

--- kOtOwls/main/forms.py
+++ kOtOwls/main/forms.py
@@ -1,4 +1,6 @@
+# forms.py
 from django import forms
 
 class TextInputForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Введите ваш текст здесь...'}))
+    image = forms.ImageField(required=False)  # Добавляем поле для загрузки изображения
--- kOtOwls/main/views.py
+++ kOtOwls/main/views.py
@@ -5,12 +5,17 @@ from .forms import TextInputForm
 # Create your views here.
 def index(request):
     text = None
+    latex_text = None  # Переменная для текста "Latex Формула"
+    image_url = None
     if request.method == "POST":
-        form = TextInputForm(request.POST)
+        form = TextInputForm(request.POST, request.FILES)  # Учитываем загружаемые файлы
         if form.is_valid():
             text = form.cleaned_data['text']
+            if form.cleaned_data['image']:
+                image_url = form.cleaned_data['image'].url  # Получаем URL изображения
+            latex_text = "Latex Формула"  # Устанавливаем текст, когда нажата кнопка
     else:
         form = TextInputForm()
 
-    return render(request, 'main/index.html', {'form': form, 'text': text})
+    return render(request, 'main/index.html', {'form': form, 'text': text, 'latex_text': latex_text, 'image_url': image_url})
 
