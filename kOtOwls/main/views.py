ca9dc8ace77efc99fc3e6d754f68ec1df9df33d6
Leonid <148010246+Leonid-Perkin@users.noreply.github.com>
2024-12-11 11:49:10 +0300

    Добавление ввода и обработки текста

--- kOtOwls/main/views.py
+++ kOtOwls/main/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import TextInputForm
+
 # Create your views here.
 def index(request):
-    return render(request,'main/index.html')
+    text = None
+    if request.method == "POST":
+        form = TextInputForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+    else:
+        form = TextInputForm()
+
+    return render(request, 'main/index.html', {'form': form, 'text': text})
+
