abfd13f6eb167073a98fbce3aa3748c68ecdd2ca
Leonid <148010246+Leonid-Perkin@users.noreply.github.com>
2024-12-11 13:17:19 +0300

    Исправление загрузки фото

--- kOtOwls/kOtOwls/settings.py
+++ kOtOwls/kOtOwls/settings.py
@@ -116,13 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
-# Папки, где Django будет искать статические файлы во время разработки
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Папка "static" в корне проекта
-]
-
+STATIC_URL = 'static/'
+MEDIA_URL = '/media/'  # URL для доступа к медиа-файлам
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- kOtOwls/main/views.py
+++ kOtOwls/main/views.py
@@ -1,21 +1,27 @@
+# views.py
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 from .forms import TextInputForm
 
-# Create your views here.
 def index(request):
     text = None
     latex_text = None  # Переменная для текста "Latex Формула"
     image_url = None
+
     if request.method == "POST":
         form = TextInputForm(request.POST, request.FILES)  # Учитываем загружаемые файлы
         if form.is_valid():
             text = form.cleaned_data['text']
+            
+            # Обрабатываем загруженное изображение
             if form.cleaned_data['image']:
-                image_url = form.cleaned_data['image'].url  # Получаем URL изображения
+                image = form.cleaned_data['image']
+                fs = FileSystemStorage()  # Создаем объект FileSystemStorage для сохранения
+                filename = fs.save(image.name, image)  # Сохраняем изображение
+                image_url = fs.url(filename)  # Получаем URL изображения
+
             latex_text = "Latex Формула"  # Устанавливаем текст, когда нажата кнопка
     else:
         form = TextInputForm()
 
     return render(request, 'main/index.html', {'form': form, 'text': text, 'latex_text': latex_text, 'image_url': image_url})
-
