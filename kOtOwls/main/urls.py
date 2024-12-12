921a37a6a6f7939cef107ce59e4c19209d9b880c
Leonid Perkin <perkin.leonid@yandex.ru>
2024-12-12 20:13:14 +0300

    Добавлено новое представление для загрузки фото и обновлено меню навигации

--- kOtOwls/main/urls.py
+++ kOtOwls/main/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('phototolatex/', views.phototolatex)
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- kOtOwls/main/views.py
+++ kOtOwls/main/views.py
@@ -1,27 +1,41 @@
 # views.py
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
-from .forms import TextInputForm
+from .forms import *
 
 def index(request):
     text = None
-    latex_text = None  # Переменная для текста "Latex Формула"
+    latex_text = None 
     image_url = None
 
     if request.method == "POST":
-        form = TextInputForm(request.POST, request.FILES)  # Учитываем загружаемые файлы
+        form = TextInputForm(request.POST, request.FILES)
         if form.is_valid():
             text = form.cleaned_data['text']
-            
-            # Обрабатываем загруженное изображение
-            if form.cleaned_data['image']:
-                image = form.cleaned_data['image']
-                fs = FileSystemStorage()  # Создаем объект FileSystemStorage для сохранения
-                filename = fs.save(image.name, image)  # Сохраняем изображение
-                image_url = fs.url(filename)  # Получаем URL изображения
+            if 'image' in request.FILES:
+                image = request.FILES['image']
+                fs = FileSystemStorage()
+                filename = fs.save(image.name, image)
+                image_url = fs.url(filename)
 
-            latex_text = "Latex Формула"  # Устанавливаем текст, когда нажата кнопка
+            latex_text = "Latex Формула"
     else:
         form = TextInputForm()
 
     return render(request, 'main/index.html', {'form': form, 'text': text, 'latex_text': latex_text, 'image_url': image_url})
+
+def phototolatex(request):
+    latex_text = None
+    image_url = None
+    if request.method == "POST":
+        form = PhotoInputForm(request.POST, request.FILES)
+        if form.is_valid():
+            if 'image' in request.FILES:
+                image = request.FILES['image']
+                fs = FileSystemStorage()
+                filename = fs.save(image.name, image)
+                image_url = fs.url(filename)
+            latex_text = "Latex Формула"
+    else:
+        form = PhotoInputForm()
+    return render(request, 'main/phototolatex.html', {'form': form, 'latex_text': latex_text, 'image_url': image_url})
