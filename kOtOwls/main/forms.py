ab99fd76c924f735ff321ed3773f1cb6630b9198
Leonid Perkin <perkin.leonid@yandex.ru>
2024-12-12 23:17:08 +0300

    Подготовлена заготовка для texttolatex, внесены основные изменения в файлы forms, urls, views, а также добавлен пункт меню "Текст в LaTeX".

--- kOtOwls/main/forms.py
+++ kOtOwls/main/forms.py
@@ -3,6 +3,5 @@ from django import forms
 
 class TextInputForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Введите ваш текст здесь...'}))
-    image = forms.ImageField(required=False)
 class PhotoInputForm(forms.Form):
     image = forms.ImageField()
--- kOtOwls/main/views.py
+++ kOtOwls/main/views.py
@@ -31,25 +31,8 @@ def extract_latex_from_image(image_path):
     
 
 def index(request):
-    text = None
-    latex_text = None 
-    image_url = None
-
-    if request.method == "POST":
-        form = TextInputForm(request.POST, request.FILES)
-        if form.is_valid():
-            text = form.cleaned_data['text']
-            if 'image' in request.FILES:
-                image = request.FILES['image']
-                fs = FileSystemStorage()
-                filename = fs.save(image.name, image)
-                image_url = fs.url(filename)
-
-            latex_text = "Latex Формула"
-    else:
-        form = TextInputForm()
-
-    return render(request, 'main/index.html', {'form': form, 'text': text, 'latex_text': latex_text, 'image_url': image_url})
+    pass
+    return render(request, 'main/index.html')
 
 def phototolatex(request):
     latex_text = None
@@ -67,3 +50,13 @@ def phototolatex(request):
     else:
         form = PhotoInputForm()
     return render(request, 'main/phototolatex.html', {'form': form, 'latex_text': latex_text, 'image_url': image_url})
+
+def texttolatex(request):
+    text = None
+    if request.method == "POST":
+        form = TextInputForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+    else:
+        form = TextInputForm()
+    return render(request, 'main/texttolatex.html', {'form': form, 'text': text})
