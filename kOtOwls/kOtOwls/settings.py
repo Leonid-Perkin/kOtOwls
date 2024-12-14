a642dd90d1796026dbd4cb6a389b51ef2a3c81b2
Leonid <148010246+Leonid-Perkin@users.noreply.github.com>
2024-12-14 20:42:21 +0300

    Add Docker support and update settings for development

--- kOtOwls/kOtOwls/settings.py
+++ kOtOwls/kOtOwls/settings.py
@@ -9,27 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
 from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-^m-ov0q$!k3po6gr#1+cl=g)ro#r)jp=$32+m9h3#%s-arx27k'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
+ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'main',
     'django.contrib.admin',
