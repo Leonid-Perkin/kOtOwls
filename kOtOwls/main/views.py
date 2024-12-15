16751e54717be153b6e31e760041c8039e933d7f
Leonid <148010246+Leonid-Perkin@users.noreply.github.com>
2024-12-15 05:23:34 +0300

    Add documentation page and update header menu for easy access

--- kOtOwls/main/views.py
+++ kOtOwls/main/views.py
@@ -21,16 +21,25 @@ from transformers import AutoTokenizer, AutoModel
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 model = AutoModel.from_pretrained("bert-base-uncased")
 def connect_to_db():
-    """Подключение к базе данных SQLite"""
+    """
+    Establishes a connection to the SQLite database.
+
+    Returns:
+        sqlite3.Connection: A connection object to the SQLite database.
+    """
     connection = sqlite3.connect('main/formulas.db')
     return connection
 
 def save_formula_to_db(latex_text):
-    """Сохранение формулы в базу данных с полем legend='no'"""
+    """
+    Save a LaTeX formula to the database with a default legend value of 'no'.
+    Args:
+        latex_text (str): The LaTeX formula to be saved.
+    Returns:
+        None
+    """
     connection = connect_to_db()
     cursor = connection.cursor()
-
-    # Проверка наличия таблицы перед её созданием
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS formulas (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -38,28 +47,51 @@ def save_formula_to_db(latex_text):
             legend TEXT
         )
     ''')
-
-    # SQL запрос для вставки данных
     cursor.execute('''
         INSERT INTO formulas (formula, legend)
         VALUES (?, ?)
     ''', (latex_text, 'no'))
-
     connection.commit()
     connection.close()
 def fetch_formulas(connection):
-    """Извлекает формулы и их легенды из базы данных SQLite"""
+    """
+    Fetches formulas and their legends from the SQLite database.
+
+    Args:
+        connection (sqlite3.Connection): The connection object to the SQLite database.
+
+    Returns:
+        list of tuple: A list of tuples where each tuple contains a formula and its corresponding legend.
+    """
     cursor = connection.cursor()
     cursor.execute("SELECT formula, legend FROM formulas")
     formulas = [(row[0], row[1]) for row in cursor.fetchall()]
     cursor.close()
     return formulas
 def compare_formulas(formula1, formula2):
-    """Сравнивает две формулы и возвращает коэффициент сходства с помощью difflib"""
+    """
+    Compares two formulas and returns a similarity coefficient using difflib.
+
+    Args:
+        formula1 (str): The first formula to compare.
+        formula2 (str): The second formula to compare.
+
+    Returns:
+        float: A similarity coefficient between 0 and 1, where 1 indicates identical formulas.
+    """
     similarity = difflib.SequenceMatcher(None, formula1, formula2).ratio()
     return similarity
 def neural_network_similarity(formula1, formula2):
-    """Использует нейросеть для вычисления сходства между формулами"""
+    """
+    Uses a neural network to compute the similarity between two formulas.
+
+    Args:
+        formula1 (str): The first formula to compare.
+        formula2 (str): The second formula to compare.
+
+    Returns:
+        float: A similarity score between 0 and 1, where 1 indicates identical formulas and 0 indicates no similarity.
+    """
     inputs = tokenizer([formula1, formula2], return_tensors="pt", padding=True, truncation=True)
     with torch.no_grad():
         outputs = model(**inputs)
@@ -67,7 +99,26 @@ def neural_network_similarity(formula1, formula2):
         similarity_score = cosine_similarity(embeddings.cpu().numpy())[0][1]
     return similarity_score
 def check_formula_uniqueness(input_formula, connection):
-    """Проверяет уникальность формулы"""
+    """
+    Check the uniqueness of the input formula by comparing it with existing formulas in the database.
+
+    This function uses two methods to compare the input formula with existing formulas:
+    1. A difflib-based comparison.
+    2. A neural network-based comparison.
+
+    Args:
+        input_formula (str): The formula to be checked for uniqueness.
+        connection (object): The database connection object.
+
+    Returns:
+        dict: A dictionary containing the following keys:
+            - 'max_similarity_difflib' (float): The maximum similarity percentage found using difflib.
+            - 'most_similar_formula_difflib' (str): The formula that is most similar to the input formula using difflib.
+            - 'most_similar_legend_difflib' (str): The legend associated with the most similar formula using difflib.
+            - 'max_similarity_nn' (float): The maximum similarity percentage found using the neural network.
+            - 'most_similar_formula_nn' (str): The formula that is most similar to the input formula using the neural network.
+            - 'most_similar_legend_nn' (str): The legend associated with the most similar formula using the neural network.
+    """
     formulas = fetch_formulas(connection)
     max_similarity_difflib = 0
     max_similarity_nn = 0
@@ -101,6 +152,19 @@ os.environ["PYTHONWARNINGS"] = "ignore"
 logging.getLogger("pydantic").setLevel(logging.CRITICAL)
 
 def extract_latex_from_image(image_path):
+    """
+    Extracts LaTeX formula from an image using OCR.
+
+    Args:
+        image_path (str): The file path to the image from which to extract the LaTeX formula.
+
+    Returns:
+        str: The extracted LaTeX formula. If the formula does not start with "\\begin{array}", it will be wrapped in "\\begin{equation}" and "\\end{equation}".
+        None: If an error occurs during the extraction process.
+
+    Raises:
+        Exception: If there is an error during the image processing or OCR.
+    """
     try:
         model = LatexOCR()
         image = Image.open(image_path).convert('RGB')
@@ -111,27 +175,21 @@ def extract_latex_from_image(image_path):
     except Exception as e:
         print(f"Ошибка при обработке изображения: {e}")
         return None
-    
-def preprocess_expression(expression):
-    replacements = {
-        r'\btg\b': 'tan',  # Замена tg -> tan
-        r'\bctg\b': 'cot', # Замена ctg -> cot
-        r'\bsh\b': 'sinh', # Замена sh -> sinh
-        r'\bch\b': 'cosh', # Замена ch -> cosh
-        r'\bth\b': 'tanh', # Замена th -> tanh
-    }
-
-    for pattern, replacement in replacements.items():
-        expression = re.sub(pattern, replacement, expression)
-    expression = expression.replace('^', '**').strip()
-    return expression
 
 def text_to_latex(input_text):
     """
-    Преобразует текстовые формулы в формат LaTeX.
-
-    :param input_text: Обычный текст с формулами
-    :return: Текст с формулами в формате LaTeX
+    Converts a given mathematical expression in plain text to LaTeX format.
+    This function applies a series of regex transformations to convert common
+    mathematical notations in plain text to their corresponding LaTeX representations.
+    The resulting LaTeX code is wrapped in an equation environment.
+    Args:
+        input_text (str): The input mathematical expression in plain text.
+    Returns:
+        str: The converted LaTeX formatted string wrapped in an equation environment.
+        None: If an error occurs during the conversion process.
+    Example:
+        >>> text_to_latex("2*x + sqrt(y) = 4")
+        '\\begin{equation}\n2 \\cdot x + \\sqrt{y} = 4\n\\end{equation}'
     """
     try:
         transformations = [
@@ -164,10 +222,24 @@ def text_to_latex(input_text):
     
 def latex_to_text(input_latex):
     """
-    Преобразует LaTeX формулы в обычный текст.
-
-    :param input_latex: Текст в формате LaTeX
-    :return: Обычный текст с формулами
+    Converts a LaTeX string to a plain text representation by applying a series of regex transformations.
+    Args:
+        input_latex (str): The LaTeX string to be converted.
+    Returns:
+        str: The plain text representation of the LaTeX string, or None if an error occurs.
+    Transformations:
+        - \frac{a}{b} -> a/b
+        - \int {f(x)} \, dx -> integral(f(x)) dx
+        - \sum_{i=1}^{n} {f(x)} -> sum(f(x), i=1..n)
+        - \lim_{x \to a} {f(x)} -> lim(f(x), x->a)
+        - \frac{\partial}{\partial x} {f(x)} -> partial(f(x), x)
+        - \sqrt{x} -> sqrt(x)
+        - x \cdot y -> x * y
+        - x^2 -> x^2
+        - x_1 -> x_1
+        - x = y -> x = y
+    Note:
+        The function also removes LaTeX equation environment delimiters (\begin{equation} and \end{equation}).
     """
     try:
         transformations = [
@@ -189,8 +261,6 @@ def latex_to_text(input_latex):
         text = input_latex
         for pattern, replacement in transformations:
             text = re.sub(pattern, replacement, text)
-        
-        # Удаляем LaTeX-среду для уравнений
         text = re.sub(r'\\begin\{equation\}\n*', '', text)
         text = re.sub(r'\n*\\end\{equation\}', '', text)
         
@@ -201,9 +271,31 @@ def latex_to_text(input_latex):
 
 
 def index(request):
+    """
+    Handle the request to the index page and render the 'main/index.html' template.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered 'main/index.html' template.
+    """
     return render(request, 'main/index.html')
 
 def phototolatex(request):
+    """
+    Handle the photo to LaTeX conversion request.
+
+    This view handles both GET and POST requests. For GET requests, it initializes an empty form.
+    For POST requests, it processes the uploaded image, saves it to the file system, extracts LaTeX
+    text from the image, and returns the result along with the form and image URL.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered HTML response with the form, extracted LaTeX text, and image URL.
+    """
     latex_text = None
     image_url = None
     if request.method == "POST":
@@ -221,6 +313,19 @@ def phototolatex(request):
     return render(request, 'main/phototolatex.html', {'form': form, 'latex_text': latex_text, 'image_url': image_url})
 
 def texttolatex(request):
+    """
+    Handle the text to LaTeX conversion form submission.
+
+    This view function processes a form submission for converting text to LaTeX.
+    If the request method is POST, it validates the form and converts the input text
+    to LaTeX format. If the request method is GET, it initializes an empty form.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered HTML response with the form and the converted text (if any).
+    """
     text = None
     if request.method == "POST":
         form = TextInputForm(request.POST)
@@ -231,14 +336,34 @@ def texttolatex(request):
     return render(request, 'main/texttolatex.html', {'form': form, 'text': text})
 
 def team(request):
+    """
+    Handles the request to display the team page.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered team page.
+    """
     return render(request,'main/team.html')
 
 def antiplagiat(request):
+    """
+    Handles the anti-plagiarism functionality by processing image uploads and text inputs.
+    This view supports two forms:
+    1. PhotoInputForm: For uploading an image containing LaTeX formulas.
+    2. TextInputForm: For entering text that will be converted to LaTeX.
+    Depending on the form submitted, it extracts LaTeX from the image or converts text to LaTeX,
+    checks the uniqueness of the formula in the database, and saves the formula if it is unique.
+    Args:
+        request (HttpRequest): The HTTP request object.
+    Returns:
+        HttpResponse: The rendered HTML page with the forms and results.
+    """
     latex_text = None
     image_url = None
     text = None
     results = None
-    # Обработка формы для загрузки изображения
     if request.method == "POST" and 'submit_image' in request.POST:
         form = PhotoInputForm(request.POST, request.FILES)
         if form.is_valid():
@@ -257,8 +382,6 @@ def antiplagiat(request):
                     save_formula_to_db(latex_text)
     else:
         form = PhotoInputForm()
-
-    # Обработка формы для ввода текста
     text = None
     if request.method == "POST":
         form1 = TextInputForm(request.POST)
@@ -283,6 +406,25 @@ def antiplagiat(request):
     })
 
 def latextotext(request):
+    """
+    Handle the conversion of LaTeX input to plain text.
+
+    This view function processes a POST request containing LaTeX input,
+    converts it to plain text, and renders the result in a template.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered HTML response with the form and converted text.
+
+    Template:
+        main/latextotext.html
+
+    Context:
+        form (TextInputForm): The form for inputting LaTeX text.
+        text (str or None): The converted plain text, or None if the form is not valid or not submitted.
+    """
     text = None
     if request.method == "POST":
         form = TextInputForm(request.POST)
@@ -291,3 +433,15 @@ def latextotext(request):
     else:
         form = TextInputForm()
     return render(request, 'main/latextotext.html', {'form': form, 'text': text})
+
+def doc(request):
+    """
+    Handles the HTTP request for the documentation page.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered HTML page for the documentation.
+    """
+    return render(request,'main/doc.html')
